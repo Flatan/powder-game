49d94574658ff23f31e218880cc927e980f12aa2
Andy Jackson <amjack100@gmail.com>
2021-05-31 09:05:40 -0400

    expose board

--- src/powder/Application.java
+++ src/powder/Application.java
@@ -3,40 +3,41 @@ package powder;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-
 // Test comment
 
 //Better test comment
 /**
- * Application 
+ * Application
  *
  * This class is the main entry point of the application
  *
  */
 public class Application extends JFrame {
 
+    public static Board board = new Board();
+
     public Application() {
         initUI();
     }
-    
+
     // Initialize a board object and set some window settings
     private void initUI() {
-        
-        add(new Board());
 
+        // add(new Board());
+
+        add(board);
         setResizable(false);
         pack();
-        
-        setTitle("Powder Game");    
+
+        setTitle("Powder Game");
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
-    
 
     // Application entry point
     public static void main(String[] args) {
-        
+
         EventQueue.invokeLater(() -> {
             JFrame ex = new Application();
             ex.setVisible(true);
