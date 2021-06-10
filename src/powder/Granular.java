f4227f25a6debe68e6c5b7bc2d1817e466f00db5
Andy Jackson <amjack100@gmail.com>
2021-06-10 08:30:10 -0400

    restore collide() just in case

--- src/powder/Granular.java
+++ src/powder/Granular.java
@@ -1,6 +1,7 @@
 package powder;
 
 import java.awt.Color;
+import java.util.function.*;
 
 import math.Vector2D;
 
@@ -13,19 +14,44 @@ import math.Vector2D;
 
 public class Granular extends Particle {
 
-	Granular(int x, int y) {
-		super(x, y, Color.white);
-		vel.y = -1;
-		giveVel = new Vector2D(0.5, 0.5);
-	}
+  Granular(int x, int y) {
+    super(x, y, Color.white);
+    vel.y = -1;
+    giveVel = new Vector2D(0.5, 0.5);
+  }
 
-	// Good ole particle movin method
-	@Override
-	public void update() {
+  // Good ole particle movin method
+  @Override
+  public void update() {
 
-		if (!updated) {
-			updated = true;
+    if (!updated) {
+      updated = true;
 
-		}
-	}
+    }
+  }
+
+  /**
+   * Calculates collision between two particles
+   * 
+   * @param other The other particle
+   * @param cR    Coefficient of Restitution. Should be between 0-1. Or SLIGHTLY
+   *              higher for fun bouncy effect
+   */
+  public void collide(Particle other, double cR) {
+
+    if (other.dynamic) {
+      BiFunction<Double, Double, Double> newVel = (a, b) -> (cR * (b - a) + a + b) / 2;
+
+      double tempVelX = vel.x;
+      double tempVelY = vel.y;
+
+      vel.x = newVel.apply(vel.x, other.vel.x);
+      vel.y = newVel.apply(vel.y, other.vel.y);
+
+      other.vel.x = newVel.apply(other.vel.x, tempVelX);
+      other.vel.y = newVel.apply(other.vel.y, tempVelY);
+    } else
+      vel = new Vector2D(0, 0);
+
+  }
 }
